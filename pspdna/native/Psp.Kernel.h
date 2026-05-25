08ba01c6dddcff7094d7c11bd99b20c6d8066780
kylecods <klrotich417@gmail.com>
2026-05-25 18:45:10 +0300

    refactor runtime

--- pspdna/native/Psp.Kernel.h
+++
@@ -1,9 +0,0 @@
-
-#if !defined(__PSP_KERNEL_H)
-#define __PSP_KERNEL_H
-
-#include "Types.h"
-
-tAsyncCall* Psp_Kernel_nativeExitGame(PTR pThis_, PTR pParams, PTR pReturnValue);
-
-#endif
--- pspdna/native/callback.h
+++
@@ -1,7 +0,0 @@
-#ifndef COMMON_CALLBACK_H
-#define COMMON_CALLBACK_H
-
-int isRunning();
-int setupExitCallback();
-
-#endif 
